4d6e5b4bfa2f7ceec9fe3d1cdcf12d6a0723dec4
Abhiroop Sarkar <asiamgenius@gmail.com>
2020-07-28 16:47:40 +0200

    Start with the expression language

---
+++ experiments/cam/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for cam
+
+## Unreleased changes
---
+++ experiments/cam/app/CAM.hs
@@ -0,0 +1,29 @@
+module CAM where
+
+type Var = String
+
+data Exp = Var Var  -- variable
+         | Sys Sys  -- Primops
+         | Void     -- Empty Tuple
+         | Pair Exp Exp    -- Pair
+         | Con  Exp        -- Constructed Value
+         | App Exp Exp     -- Function application
+         | Lam Pat Exp     -- Lambda Abstraction
+         | If Exp Exp Exp  -- if then else
+         | Let Pat Exp Exp -- Let bindings
+         | Letrec Pat Exp Exp -- letrec
+         | Case Exp [(Exp, Exp)]
+         --            ^    ^
+         --     constructor |
+         --              resulting expression
+         deriving (Ord, Show, Eq)
+
+data Sys = Sys2 Exp Exp
+         | Sys1  Exp
+         deriving (Ord, Show, Eq)
+
+data Pat = PatVar Var
+         | Empty
+         | PatPair Pat Pat
+         | As Var Pat      -- var `as` pat; equivalent to @ in Haskell
+         deriving (Ord, Show, Eq)
