82204fb5fee41d8933f6e6daf7b1f61d7a208ba6
Abhiroop Sarkar <asiamgenius@gmail.com>
2020-07-29 12:46:24 +0200

    Add a few instructions

--- experiments/cam/app/CAM.hs
+++ experiments/cam/app/CAM.hs
@@ -18,8 +18,10 @@ data Exp = Var Var  -- variable
          --              resulting expression
          deriving (Ord, Show, Eq)
 
-data Sys = Sys2 Exp Exp
-         | Sys1 Exp
+data Sys = Sys2 Exp Exp -- BinOp
+         | Sys1 Exp     -- UnaryOp
+         | LInt Int     -- Int s(0) in cam
+         | LBool Bool   -- Bool s(0) in cam
          deriving (Ord, Show, Eq)
 
 data Pat = PatVar Var
@@ -27,3 +29,19 @@ data Pat = PatVar Var
          | PatPair Pat Pat
          | As Var Pat      -- var `as` pat; equivalent to @ in Haskell
          deriving (Ord, Show, Eq)
+
+data Instructions
+   = ACC   Int  -- access the nth component of the environment register
+   | QUOTE Sys  -- load immediate i.e `li` from the code area to environment
+   | PUSH       -- copy content of register to stack
+   | SWAP       -- interchange between register and topmost element of stack
+   | CUR   Exp  -- build a closure with Exp and the environment register
+   | RETURN     -- return from a subroutine call
+
+-- NOTE:
+{-
+1. Use the stack for intermediate storage of environment;
+   Always PUSH before beginning computation
+2. BinOp (s(2)) expects first argument on stack and second on register
+
+-}
