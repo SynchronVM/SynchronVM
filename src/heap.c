bdd59e659af68db469c048fb57eb23389e799c44
Joel Svensson <svenssonjoel@yahoo.se>
2020-07-29 14:51:27 +0200

    sketching some more

---
+++ src/heap.c
@@ -0,0 +1,39 @@
+/**********************************************************************************/
+/* MIT License									  */
+/* 										  */
+/* Copyright (c) 2020 Joel Svensson             				  */
+/* 										  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy	  */
+/* of this software and associated documentation files (the "Software"), to deal  */
+/* in the Software without restriction, including without limitation the rights	  */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell	  */
+/* copies of the Software, and to permit persons to whom the Software is	  */
+/* furnished to do so, subject to the following conditions:			  */
+/* 										  */
+/* The above copyright notice and this permission notice shall be included in all */
+/* copies or substantial portions of the Software.				  */
+/* 										  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR	  */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,	  */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE	  */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER	  */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  */
+/* SOFTWARE.									  */
+/**********************************************************************************/
+
+#include <heap.h>
+
+#include <stdlib.h> // later remove this include when no memory "malloced" in this file. 
+
+heap_cell_t* heap_init(unsigned int n_cells) {
+
+  heap_cell_t* heap = malloc(sizeof(heap_cell_t) * n_cells);
+
+  if (heap) {
+    for (unsigned int i = 0; i < n_cells; i ++) {
+      heap[i].flags = 0;
+    }
+  }
+  return heap;
+}
