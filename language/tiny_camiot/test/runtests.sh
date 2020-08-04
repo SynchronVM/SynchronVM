057de0f90f4616f42ef4e9be69207c956aa5dd5a
Joel Svensson <svenssonjoel@yahoo.se>
2020-08-04 21:36:07 +0200

    added some tests and a pair type to bnfc

--- language/tiny_camiot/test/runtests.sh
+++ language/tiny_camiot/test/runtests.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+success_count=0
+fail_count=0;
+failing_tests="failed tests: \n"
+
 for file in `ls`
 do
 	if [ $(basename $file) != $(basename $0) ]
@@ -8,14 +12,25 @@ do
 		res=$?
 	        if [ "$res" -eq 0 ]
                 then
-			echo "file: $file parsed successfully"
+		    success_count=$((success_count+1))
+		    echo "file: $file parsed successfully"
 	        fi
 		if [ "$res" -eq 1 ]
 		then
-			echo "\noops!"
-			echo "file: $file did not parse successfully"
-			echo $var
-			break
+		    fail_count=$((fail_count+1))
+		    failing_tests="$failing_tests $file \n"
+		    echo "\noops!"
+		    echo "file: $file did not parse successfully"
+		    echo $var
 		fi
         fi
 done
+
+echo Tests passed: $success_count
+echo Tests failed: $fail_count
+
+if [ $fail_count -gt 0 ]
+then
+    echo $failing_tests
+    exit 1
+fi
