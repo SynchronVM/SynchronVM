ea33807e96141c757d5eba5a5c4f40ffb5bf1995
Joel Svensson <svenssonjoel@yahoo.se>
2020-08-20 08:55:33 +0200

    added forgotten test files

---
+++ tests/heap_test_2.c
@@ -0,0 +1,98 @@
+/**********************************************************************************/
+/* MIT License									  */
+/* 										  */
+/* Copyright (c) 2020 Joel Svensson             				  */
+/* 										  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy	  */
+/* of this software and associated documentation files (the "Software"), to deal  */
+/* in the Software without restriction, including without limitation the rights	  */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell	  */
+/* copies of the Software, and to permit persons to whom the Software is	  */
+/* furnished to do so, subject to the following conditions:			  */
+/* 										  */
+/* The above copyright notice and this permission notice shall be included in all */
+/* copies or substantial portions of the Software.				  */
+/* 										  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR	  */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,	  */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE	  */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER	  */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  */
+/* SOFTWARE.									  */
+/**********************************************************************************/
+
+#include <heap.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv) {
+
+  int r = heap_init(1000);
+
+  if (r) {
+    printf("heap_init: OK!\n");
+  } else {
+    printf("heap_init: Failed!\n");
+    return 0;
+  }
+
+  unsigned int n = heap_num_free();
+
+  if (n == 1000) {
+    printf("heap_num_free 1000: OK!\n");
+  } else {
+    printf("heap_num_free 1000: Failed!\n");
+    printf("Free elements: %u\n", n);
+    return 0;
+  }
+
+  for (int i = 0; i < 125; i ++) {
+    heap_allocate();
+  }
+
+  n = heap_num_free();
+
+  if (n == 875) {
+    printf("heap_num_free 875: OK!\n");
+  } else {
+    printf("heap_num_free 875: Failed!\n");
+    printf("Free elements: %u\n", n);
+    return 0;
+  }
+
+  for (int i = 0; i < 874; i ++) {
+    heap_allocate();
+  }
+
+  n = heap_num_free();
+
+  if (n == 1) {
+    printf("heap_num_free 1: OK!\n");
+  } else {
+    printf("heap_num_free 1: Failed!\n");
+    printf("Free elements: %u\n", n);
+    return 0;
+  }
+
+  heap_index ix = heap_allocate();
+  if ( ix == 999 ) {
+    printf("Last index: OK!\n");
+  } else {
+    printf("Last index: Failed!\n");
+    printf("Last index: %d\n",ix);
+    return 0;
+  }
+
+  if (heap_allocate() == HEAP_NULL) {
+    printf("Heap full: OK!\n");
+  } else {
+    printf("Heap full: Failed!\n");
+    return 0;
+  }
+
+  heap_destroy();
+  return r;
+}
