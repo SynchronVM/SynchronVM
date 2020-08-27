14896b62e52ca165e58914ed2b4984c777be09ba
Joel Svensson <svenssonjoel@yahoo.se>
2020-08-27 18:48:20 +0200

    added template for chibios experiments in directory ch_sense (it compiles)

---
+++ ch_sense/main.c
@@ -0,0 +1,107 @@
+/*
+    Copyright 2019 Joel Svensson	svenssonjoel@yahoo.se
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// File also contains code distributed as part of Chibios under license below.
+
+/*
+    ChibiOS - Copyright (C) 2006..2018 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ */
+
+#include "ctype.h"
+
+#include "ch.h"
+#include "hal.h"
+#include "chvt.h"
+#include "chtime.h"
+
+#include "usbcfg.h"
+#include "chprintf.h"
+
+BaseSequentialStream *chp = NULL;
+
+int inputline(BaseSequentialStream *chp, char *buffer, int size) {
+  int n = 0;
+  unsigned char c;
+  for (n = 0; n < size - 1; n++) {
+
+    c = streamGet(chp);
+    switch (c) {
+    case 127: /* fall through to below */
+    case '\b': /* backspace character received */
+      if (n > 0)
+        n--;
+      buffer[n] = 0;
+      streamPut(chp,0x8); /* output backspace character */
+      streamPut(chp,' ');
+      streamPut(chp,0x8);
+      n--; /* set up next iteration to deal with preceding char location */
+      break;
+    case '\n': /* fall through to \r */
+    case '\r':
+      buffer[n] = 0;
+      return n;
+    default:
+      if (isprint(c)) { /* ignore non-printable characters */
+        streamPut(chp,c);
+        buffer[n] = c;
+      } else {
+        n -= 1;
+      }
+      break;
+    }
+  }
+  buffer[size - 1] = 0;
+  return 0; // Filled up buffer without reading a linebreak
+}
+
+int main(void) {
+  halInit();
+  chSysInit();
+
+  sduObjectInit(&SDU1);
+  sduStart(&SDU1, &serusbcfg1);
+
+  /*
+   * Activates the USB driver and then the USB bus pull-up on D+.
+   * Note, a delay is inserted in order to not have to disconnect the cable
+   * after a reset.
+   */
+  usbDisconnectBus(serusbcfg1.usbp);
+  chThdSleepMilliseconds(1500);
+  usbStart(serusbcfg1.usbp, &usbcfg);
+  usbConnectBus(serusbcfg1.usbp);	
+
+  chp = (BaseSequentialStream*)&SDU1;
+  
+  while(1) {
+    chprintf(chp, "Tick!\n");
+    chThdSleepMilliseconds(500);
+  }
+
+}
