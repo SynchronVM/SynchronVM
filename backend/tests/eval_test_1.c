76d54d1d7bbe866cc0b14a68a8e77435ed613c26
Abhiroop Sarkar <asiamgenius@gmail.com>
2020-09-20 21:52:17 +0200

    Add eval for push and add tests

--- backend/tests/eval_test_1.c
+++ backend/tests/eval_test_1.c
@@ -31,6 +31,7 @@
 
 int eval_fst(vmc_t *vmc, uint8_t *bc_rest);
 int eval_snd(vmc_t *vmc, uint8_t *bc_rest);
+int eval_push(vmc_t *vmc, uint8_t *bc_rest);
 
 bool eval_fst_test(){
   heap_cell_t hc1 = { .fst = 0 }; // DUMMY CELL not used
@@ -72,6 +73,29 @@ bool eval_snd_test(){
   }
 }
 
+bool eval_push_test(){
+  cam_value_t cv = { .value = 10, .flags = 0 };
+  cam_stack_t s = { .size = 0 };
+  uint8_t *m = malloc(16);
+  int w = stack_init(&s, m, 256);
+  VM_t mockvm = { .env = cv, .stack = s };
+  vmc_t vmc = { .vm = mockvm };
+
+  cam_register_t dummyreg = { .value = 0 };
+  int i = eval_push(&vmc, NULL);
+  int j = stack_pop(&vmc.vm.stack, &dummyreg);
+  (void)i;
+  (void)j;
+  (void)w;
+  if(dummyreg.value == 10){
+    free(m);
+    return true;
+  } else {
+    free(m);
+    return false;
+  }
+}
+
 int main(int argc, char **argv) {
   (void)argc;
   (void)argv;
@@ -79,18 +103,25 @@ int main(int argc, char **argv) {
 
 
   bool t1 = eval_fst_test();
-  bool t2 = eval_snd_test();
   if (t1) {
     printf("eval_fst unit test passed\n");
     total++;
   } else {
     printf("eval_fst unit test failed\n");
   }
+  bool t2 = eval_snd_test();
   if (t2) {
     printf("eval_snd unit test passed\n");
     total++;
   } else {
     printf("eval_snd unit test failed\n");
+  }
+  bool t3 = eval_push_test();
+  if (t3) {
+    printf("eval_push unit test passed\n");
+    total++;
+  } else {
+    printf("eval_push unit test failed\n");
   }
 
   printf("Passed total : %d tests\n", total);
