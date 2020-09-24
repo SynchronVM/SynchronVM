3667f883253c1a252252f8135af2e8657f14b0f7
Abhiroop Sarkar <asiamgenius@gmail.com>
2020-09-24 21:58:58 +0200

    Add implementation of addi operation

--- backend/src/CAM.c
+++ backend/src/CAM.c
@@ -320,8 +320,17 @@ int eval_dec(vmc_t *vmc, uint8_t *bc_rest) {
 }
 
 int eval_addi(vmc_t *vmc, uint8_t *bc_rest) {
-  (void)vmc;
   (void)bc_rest;
+  cam_register_t e = vmc->vm.env;
+  cam_register_t hold_reg = { .flags = 0, .value = 0 }; // init register
+  int i = stack_pop(&vmc->vm.stack, &hold_reg);
+  if(i == 0){
+    DEBUG_PRINT(("Stack pop has failed"));
+    return -1;
+  }
+  cam_register_t final_value =
+    { .flags = 0, .value = hold_reg.value + e.value };
+  vmc->vm.env = final_value;
   return 1;
 }
 
