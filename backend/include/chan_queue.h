7e6e84566faf53b45521990f11b298d86dcbe4e6
Abhiroop Sarkar <asiamgenius@gmail.com>
2021-02-01 13:35:03 +0100

    Add a new channel queue which can hold messages as well as context ids

---
+++ backend/include/chan_queue.h
@@ -0,0 +1,50 @@
+/**********************************************************************************/
+/* MIT License									  */
+/* 										  */
+/* Copyright (c) 2020 Abhiroop Sarkar             		  */
+/* 										  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy	  */
+/* of this software and associated documentation files (the "Software"), to deal  */
+/* in the Software without restriction, including without limitation the rights	  */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell	  */
+/* copies of the Software, and to permit persons to whom the Software is	  */
+/* furnished to do so, subject to the following conditions:			  */
+/* 										  */
+/* The above copyright notice and this permission notice shall be included in all */
+/* copies or substantial portions of the Software.				  */
+/* 										  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR	  */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,	  */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE	  */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER	  */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  */
+/* SOFTWARE.									  */
+/**********************************************************************************/
+
+#ifndef __CHAN_QUEUE_H_
+#define __CHAN_QUEUE_H_
+
+#include <typedefs.h>
+
+typedef struct {
+  UUID context_id;
+  cam_value_t message;
+} chan_data_t;
+
+chan_data_t default_val = { .context_id = 255 };
+
+typedef struct {
+  chan_data_t *data;
+  int capacity;
+  int front;
+  int rear;
+} chan_queue_t;
+
+extern int chan_q_init(chan_queue_t *q, uint8_t *mem, unsigned int size_bytes);
+
+extern int chan_q_enqueue(chan_queue_t *q, chan_data_t chan_data);
+extern int chan_q_dequeue(chan_queue_t *q, chan_data_t *chan_data);
+extern int chan_q_front  (chan_queue_t *q, chan_data_t *chan_data);
+
+#endif
---
+++ backend/src/chan_queue.c
@@ -0,0 +1,94 @@
+/**********************************************************************************/
+/* MIT License									  */
+/* 										  */
+/* Copyright (c) 2020 Abhiroop Sarkar             				  */
+/* 										  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy	  */
+/* of this software and associated documentation files (the "Software"), to deal  */
+/* in the Software without restriction, including without limitation the rights	  */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell	  */
+/* copies of the Software, and to permit persons to whom the Software is	  */
+/* furnished to do so, subject to the following conditions:			  */
+/* 										  */
+/* The above copyright notice and this permission notice shall be included in all */
+/* copies or substantial portions of the Software.				  */
+/* 										  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR	  */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,	  */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE	  */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER	  */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  */
+/* SOFTWARE.									  */
+/**********************************************************************************/
+#ifdef DEBUG
+#include <stdio.h>
+# define DEBUG_PRINT(x) printf x
+#else
+# define DEBUG_PRINT(x) do {} while (0)
+#endif
+
+#include<chan_queue.h>
+
+int chan_q_init(chan_queue_t *q, uint8_t *mem, unsigned int size_bytes){
+
+  if (!mem || !q || size_bytes < 256) return 0;
+  unsigned int num_elt = size_bytes / sizeof(chan_data_t);
+  q->front = -1;
+  q->rear  = -1;
+  q->data  = (chan_data_t*)mem;
+  q->capacity = num_elt;
+  return 1;
+}
+
+int chan_q_enqueue(chan_queue_t *q, chan_data_t chan_data){
+  if ((q->front == 0 && q->rear == q->capacity-1) || (q->rear == (q->front-1)%(q->capacity-1))){
+    DEBUG_PRINT(("Queue is full\n"));
+    return -1;
+  }
+
+  else if (q->front == -1){ /* Insert First Element */
+    q->front = 0;
+    q->rear = 0;
+    q->data[q->rear] = chan_data;
+  }
+  else if (q->rear == q->capacity-1 && q->front != 0) { /* Circle around */
+    q->rear = 0;
+    q->data[q->rear] = chan_data;
+  }
+  else {
+    q->data[q->rear++] = chan_data;
+  }
+  return 1;
+}
+
+int chan_q_dequeue(chan_queue_t *q, chan_data_t *chan_data){
+  if (q->front == -1){
+    DEBUG_PRINT(("Queue is empty\n"));
+    return -1;
+  }
+
+  *chan_data = q->data[q->front];
+
+  q->data[q->front] = default_val;
+
+  if (q->front == q->rear){
+    q->front = -1;
+    q->rear = -1;
+  }
+  else if (q->front == q->capacity-1)
+    q->front = 0;
+  else
+    q->front++;
+  return 1;
+}
+
+int chan_q_front(chan_queue_t *q, chan_data_t *chan_data){
+  if (q->front == -1){
+    DEBUG_PRINT(("Queue is empty\n"));
+    return -1;
+  }
+
+  *chan_data = q->data[q->front];
+  return 1;
+}
