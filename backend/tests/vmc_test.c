45c90486ae2a939df1ccd037062c91986ccc1bd8
Joel Svensson <svenssonjoel@yahoo.se>
2021-02-03 18:34:11 +0100

    updated vmc_test.c

--- backend/tests/vmc_test.c
+++ backend/tests/vmc_test.c
@@ -326,4 +326,8 @@ int main(int argc, char **argv) {
   test_stat("vmc_run_6", &total, t6);
 
   printf("Passed total : %d/%d tests\n", total, 6);
+  if (t1 && t2 && t3 && t4 && t5 && t6) {
+    return 0;
+  }
+  return -1
 }
