482bc0bfeac7d94171399ac81ddf44f75db61514
Joel Svensson <svenssonjoel@yahoo.se>
2021-03-17 17:38:06 +0100

    update fib

--- experiments/zephyr/peng-nrf52/src/main.c
+++ experiments/zephyr/peng-nrf52/src/main.c
@@ -13,9 +13,7 @@
 #include <drivers/sensor.h>
 #include <drivers/counter.h>
 
-
-#include <peng-platform.h>
-#include <peng.h>
+#include "fib.h"
 
 /* Our own library of stuff! */
 //#include "defines.h"
@@ -66,196 +64,196 @@ uint8_t uart0_out_buffer[1024];
 /* ************************************************************ */
 
 
-typedef struct {
-    /* Procedure generic fields */;
-  ACTIVATION_RECORD_FIELDS;
-    /* void (*step)(act_t *); */
-    /* uint16_t  pc; */
-    /* act_t    *caller; */
-    /* uint16_t  children; */
-    /* uint32_t  priority; */
-    /* uint8_t   depth; */
-    /* bool      scheduled; */
-    /* Procedure specific fields */;
-    sv_int_t *r; // Procedure argument;
-    trigger_t trig1;
-} act_mywait_t;
+/* typedef struct { */
+/*     /\* Procedure generic fields *\/; */
+/*   ACTIVATION_RECORD_FIELDS; */
+/*     /\* void (*step)(act_t *); *\/ */
+/*     /\* uint16_t  pc; *\/ */
+/*     /\* act_t    *caller; *\/ */
+/*     /\* uint16_t  children; *\/ */
+/*     /\* uint32_t  priority; *\/ */
+/*     /\* uint8_t   depth; *\/ */
+/*     /\* bool      scheduled; *\/ */
+/*     /\* Procedure specific fields *\/; */
+/*     sv_int_t *r; // Procedure argument; */
+/*     trigger_t trig1; */
+/* } act_mywait_t; */
 
-act_mywait_t *enter_mywait(act_t *caller, uint32_t priority, uint8_t depth, sv_int_t *r);
-void step_mywait(act_t *gen_act);
-act_mywait_t *enter_mywait(act_t *caller, uint32_t priority, uint8_t depth, sv_int_t *r) {
-    act_mywait_t *act = (act_mywait_t *) enter(sizeof(act_mywait_t), step_mywait, caller, priority, depth);
-    act->r = r;
-    act->trig1.act = (act_t *) act;
-    return act;
-};
+/* act_mywait_t *enter_mywait(act_t *caller, uint32_t priority, uint8_t depth, sv_int_t *r); */
+/* void step_mywait(act_t *gen_act); */
+/* act_mywait_t *enter_mywait(act_t *caller, uint32_t priority, uint8_t depth, sv_int_t *r) { */
+/*     act_mywait_t *act = (act_mywait_t *) enter(sizeof(act_mywait_t), step_mywait, caller, priority, depth); */
+/*     act->r = r; */
+/*     act->trig1.act = (act_t *) act; */
+/*     return act; */
+/* }; */
 
-void step_mywait(act_t *gen_act) {
-    act_mywait_t *act = (act_mywait_t *) gen_act;
-    switch(act->pc) {
-        case 0:
-            sensitize((sv_t *) act->r, &act->trig1);
-            act->pc = 1;
-            return;
+/* void step_mywait(act_t *gen_act) { */
+/*     act_mywait_t *act = (act_mywait_t *) gen_act; */
+/*     switch(act->pc) { */
+/*         case 0: */
+/*             sensitize((sv_t *) act->r, &act->trig1); */
+/*             act->pc = 1; */
+/*             return; */
 
-        case 1:
-            desensitize(&act->trig1);
+/*         case 1: */
+/*             desensitize(&act->trig1); */
 
-        leave((act_t *) act, sizeof(act_mywait_t));
-    }
-}
+/*         leave((act_t *) act, sizeof(act_mywait_t)); */
+/*     } */
+/* } */
 
-typedef struct {
-    /* Procedure generic fields */;
-  ACTIVATION_RECORD_FIELDS;
-    /* void (*step)(act_t *); */
-    /* uint16_t  pc; */
-    /* act_t    *caller; */
-    /* uint16_t  children; */
-    /* uint32_t  priority; */
-    /* uint8_t   depth; */
-    /* bool      scheduled; */
-    /* Procedure specific fields */;
-    sv_int_t *r1; // Procedure argument;
-    sv_int_t *r2; // Procedure argument;
-    sv_int_t *r; // Procedure argument;
-    sv_int_t v1; // Declared at line 16, column 5 in file Fib.hs;
-    sv_int_t v2; // Declared at line 17, column 5 in file Fib.hs;
-} act_mysum_t;
+/* typedef struct { */
+/*     /\* Procedure generic fields *\/; */
+/*   ACTIVATION_RECORD_FIELDS; */
+/*     /\* void (*step)(act_t *); *\/ */
+/*     /\* uint16_t  pc; *\/ */
+/*     /\* act_t    *caller; *\/ */
+/*     /\* uint16_t  children; *\/ */
+/*     /\* uint32_t  priority; *\/ */
+/*     /\* uint8_t   depth; *\/ */
+/*     /\* bool      scheduled; *\/ */
+/*     /\* Procedure specific fields *\/; */
+/*     sv_int_t *r1; // Procedure argument; */
+/*     sv_int_t *r2; // Procedure argument; */
+/*     sv_int_t *r; // Procedure argument; */
+/*     sv_int_t v1; // Declared at line 16, column 5 in file Fib.hs; */
+/*     sv_int_t v2; // Declared at line 17, column 5 in file Fib.hs; */
+/* } act_mysum_t; */
 
-act_mysum_t *enter_mysum(act_t *caller, uint32_t priority, uint8_t depth, sv_int_t *r1, sv_int_t *r2, sv_int_t *r);
-void step_mysum(act_t *gen_act);
-act_mysum_t *enter_mysum(act_t *caller, uint32_t priority, uint8_t depth, sv_int_t *r1, sv_int_t *r2, sv_int_t *r) {
-    act_mysum_t *act = (act_mysum_t *) enter(sizeof(act_mysum_t), step_mysum, caller, priority, depth);
-    act->r1 = r1;
-    act->r2 = r2;
-    act->r = r;
-    return act;
-};
+/* act_mysum_t *enter_mysum(act_t *caller, uint32_t priority, uint8_t depth, sv_int_t *r1, sv_int_t *r2, sv_int_t *r); */
+/* void step_mysum(act_t *gen_act); */
+/* act_mysum_t *enter_mysum(act_t *caller, uint32_t priority, uint8_t depth, sv_int_t *r1, sv_int_t *r2, sv_int_t *r) { */
+/*     act_mysum_t *act = (act_mysum_t *) enter(sizeof(act_mysum_t), step_mysum, caller, priority, depth); */
+/*     act->r1 = r1; */
+/*     act->r2 = r2; */
+/*     act->r = r; */
+/*     return act; */
+/* }; */
 
-void step_mysum(act_t *gen_act) {
-    act_mysum_t *act = (act_mysum_t *) gen_act;
-    switch(act->pc) {
-        case 0:
-            {
-            uint8_t new_depth = act->depth - 1;
-            uint32_t pinc = 1 << new_depth;
-            uint32_t new_priority = act->priority;
-            fork_routine((act_t *) enter_mywait( (act_t *) act
-                                               , new_priority
-                                               , new_depth
-                                               , act->r1));
-            new_priority += pinc;
-            fork_routine((act_t *) enter_mywait( (act_t *) act
-                                               , new_priority
-                                               , new_depth
-                                               , act->r2));
-            }
-            act->pc = 1;
-            return;
+/* void step_mysum(act_t *gen_act) { */
+/*     act_mysum_t *act = (act_mysum_t *) gen_act; */
+/*     switch(act->pc) { */
+/*         case 0: */
+/*             { */
+/*             uint8_t new_depth = act->depth - 1; */
+/*             uint32_t pinc = 1 << new_depth; */
+/*             uint32_t new_priority = act->priority; */
+/*             fork_routine((act_t *) enter_mywait( (act_t *) act */
+/*                                                , new_priority */
+/*                                                , new_depth */
+/*                                                , act->r1)); */
+/*             new_priority += pinc; */
+/*             fork_routine((act_t *) enter_mywait( (act_t *) act */
+/*                                                , new_priority */
+/*                                                , new_depth */
+/*                                                , act->r2)); */
+/*             } */
+/*             act->pc = 1; */
+/*             return; */
 
-        case 1:
-            assign_int(&act->v1, act->priority, act->r1->value);
-            assign_int(&act->v2, act->priority, act->r2->value);
-            later_int(act->r, now + 1, (act->v1.value) + (act->v2.value));
+/*         case 1: */
+/*             assign_int(&act->v1, act->priority, act->r1->value); */
+/*             assign_int(&act->v2, act->priority, act->r2->value); */
+/*             later_int(act->r, now + 1, (act->v1.value) + (act->v2.value)); */
 
-	leave((act_t *) act, sizeof(act_mysum_t));
-    }
-}
+/* 	leave((act_t *) act, sizeof(act_mysum_t)); */
+/*     } */
+/* } */
 
-typedef struct {
-    /* Procedure generic fields */;
-   ACTIVATION_RECORD_FIELDS;
-  //  void (*step)(act_t *);
-  //  uint16_t  pc;
-  //  act_t    *caller;
-  //  uint16_t  children;
-  //  uint32_t  priority;
-  //  uint8_t   depth;
-  //  bool      scheduled;
-    /* Procedure specific fields */;
-    sv_int_t n; // Procedure argument;
-    sv_int_t *r; // Procedure argument;
-    sv_int_t *r1; // Declared at line 22, column 5 in file Fib.hs;
-    sv_int_t *r2; // Declared at line 23, column 5 in file Fib.hs;
-} act_myfib_t;
+/* typedef struct { */
+/*     /\* Procedure generic fields *\/; */
+/*    ACTIVATION_RECORD_FIELDS; */
+/*   //  void (*step)(act_t *); */
+/*   //  uint16_t  pc; */
+/*   //  act_t    *caller; */
+/*   //  uint16_t  children; */
+/*   //  uint32_t  priority; */
+/*   //  uint8_t   depth; */
+/*   //  bool      scheduled; */
+/*     /\* Procedure specific fields *\/; */
+/*     sv_int_t n; // Procedure argument; */
+/*     sv_int_t *r; // Procedure argument; */
+/*     sv_int_t *r1; // Declared at line 22, column 5 in file Fib.hs; */
+/*     sv_int_t *r2; // Declared at line 23, column 5 in file Fib.hs; */
+/* } act_myfib_t; */
 
-act_myfib_t *enter_myfib(act_t *caller, uint32_t priority, uint8_t depth, int n, sv_int_t *r);
-void step_myfib(act_t *gen_act);
+/* act_myfib_t *enter_myfib(act_t *caller, uint32_t priority, uint8_t depth, int n, sv_int_t *r); */
+/* void step_myfib(act_t *gen_act); */
 
 
-sv_int_t r1_static;
-sv_int_t r2_static;
+/* sv_int_t r1_static; */
+/* sv_int_t r2_static; */
 
-act_myfib_t *enter_myfib(act_t *caller, uint32_t priority, uint8_t depth, int n, sv_int_t *r) {
-    act_myfib_t *act = (act_myfib_t *) enter(sizeof(act_myfib_t), step_myfib, caller, priority, depth);
-    initialize_int(&act->n);
-    assign_int(&act->n, act->priority, n);
-    act->r = r;
-    //PRINT("enter_myfib: Allocating space for two sv_int\r\n");
-    act->r1 = (sv_int_t *) malloc(sizeof(sv_int_t));
-    act->r2 = (sv_int_t *) malloc(sizeof(sv_int_t));
-    //PRINT("enter_myfib: returning\r\n");
-    return act;
-};
+/* act_myfib_t *enter_myfib(act_t *caller, uint32_t priority, uint8_t depth, int n, sv_int_t *r) { */
+/*     act_myfib_t *act = (act_myfib_t *) enter(sizeof(act_myfib_t), step_myfib, caller, priority, depth); */
+/*     initialize_int(&act->n); */
+/*     assign_int(&act->n, act->priority, n); */
+/*     act->r = r; */
+/*     //PRINT("enter_myfib: Allocating space for two sv_int\r\n"); */
+/*     act->r1 = (sv_int_t *) malloc(sizeof(sv_int_t)); */
+/*     act->r2 = (sv_int_t *) malloc(sizeof(sv_int_t)); */
+/*     //PRINT("enter_myfib: returning\r\n"); */
+/*     return act; */
+/* }; */
 
-int steps = 0; 
+/* int steps = 0;  */
 
-void step_myfib(act_t *gen_act) {
+/* void step_myfib(act_t *gen_act) { */
 
-  //PRINT("step_myfib\r\n");
+/*   //PRINT("step_myfib\r\n"); */
   
-  steps ++; 
-    act_myfib_t *act = (act_myfib_t *) gen_act;
-    switch(act->pc) {
-        case 0:
-            initialize_int(act->r1);
-            assign_int(act->r1, act->priority, 0);
-            initialize_int(act->r2);
-            assign_int(act->r2, act->priority, 0);
-            if (!((act->n.value) < (2))) goto L0;
-            later_int(act->r, now + 1, 1);
-            goto L1;
+/*   steps ++;  */
+/*     act_myfib_t *act = (act_myfib_t *) gen_act; */
+/*     switch(act->pc) { */
+/*         case 0: */
+/*             initialize_int(act->r1); */
+/*             assign_int(act->r1, act->priority, 0); */
+/*             initialize_int(act->r2); */
+/*             assign_int(act->r2, act->priority, 0); */
+/*             if (!((act->n.value) < (2))) goto L0; */
+/*             later_int(act->r, now + 1, 1); */
+/*             goto L1; */
 
-        L0:
-            {
-            uint8_t new_depth = act->depth - 1;
-            uint32_t pinc = 1 << new_depth;
-            uint32_t new_priority = act->priority;
-            fork_routine((act_t *) enter_myfib( (act_t *) act
-                                              , new_priority
-                                              , new_depth
-                                              , (act->n.value) - (1)
-                                              , act->r1));
-            new_priority += pinc;
-            fork_routine((act_t *) enter_myfib( (act_t *) act
-                                              , new_priority
-                                              , new_depth
-                                              , (act->n.value) - (2)
-                                              , act->r2));
-            new_priority += pinc;
-            fork_routine((act_t *) enter_mysum( (act_t *) act
-                                              , new_priority
-                                              , new_depth
-                                              , act->r1
-                                              , act->r2
-                                              , act->r));
-            }
-            act->pc = 1;
-	    //PRINT("step_myfib: r1 = %d | r2 = %d | r = %d \r\n",act->r1->value,act->r2->value,act->r->value);
-	    //uart_printf(&uart0,"r1 = %d | r2 = %d | r = %d \r\n",act->r1->value,act->r2->value,act->r->value);
-            return;
+/*         L0: */
+/*             { */
+/*             uint8_t new_depth = act->depth - 1; */
+/*             uint32_t pinc = 1 << new_depth; */
+/*             uint32_t new_priority = act->priority; */
+/*             fork_routine((act_t *) enter_myfib( (act_t *) act */
+/*                                               , new_priority */
+/*                                               , new_depth */
+/*                                               , (act->n.value) - (1) */
+/*                                               , act->r1)); */
+/*             new_priority += pinc; */
+/*             fork_routine((act_t *) enter_myfib( (act_t *) act */
+/*                                               , new_priority */
+/*                                               , new_depth */
+/*                                               , (act->n.value) - (2) */
+/*                                               , act->r2)); */
+/*             new_priority += pinc; */
+/*             fork_routine((act_t *) enter_mysum( (act_t *) act */
+/*                                               , new_priority */
+/*                                               , new_depth */
+/*                                               , act->r1 */
+/*                                               , act->r2 */
+/*                                               , act->r)); */
+/*             } */
+/*             act->pc = 1; */
+/* 	    //PRINT("step_myfib: r1 = %d | r2 = %d | r = %d \r\n",act->r1->value,act->r2->value,act->r->value); */
+/* 	    //uart_printf(&uart0,"r1 = %d | r2 = %d | r = %d \r\n",act->r1->value,act->r2->value,act->r->value); */
+/*             return; */
 
-    case 1:
+/*     case 1: */
       
 
-    L1:
-      PRINT("LEAVING\r\n");
-      free(act->r1);
-      free(act->r2);
-      leave((act_t *) act, sizeof(act_myfib_t));
-    }
-}
+/*     L1: */
+/*       PRINT("LEAVING\r\n"); */
+/*       free(act->r1); */
+/*       free(act->r2); */
+/*       leave((act_t *) act, sizeof(act_myfib_t)); */
+/*     } */
+/* } */
 
 
 void top_return(act_t *act)
@@ -352,7 +350,6 @@ void tick_thread_main(void * a, void* b, void *c) {
   
 }
 
-
 void start_tick_thread(void) {
 
   k_thread_create(&tick_thread, tick_thread_stack,
@@ -361,12 +358,6 @@ void start_tick_thread(void) {
 		  NULL, NULL, NULL,
 		  5, 0, K_NO_WAIT);
 }
-
-
-
-
-
-
 
 /* ************************************************************ */
 /* MAIN                                                         */
