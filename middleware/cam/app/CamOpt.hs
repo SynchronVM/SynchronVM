466f7664d9c98812dd6b35185039683390075b3e
Abhiroop Sarkar <asiamgenius@gmail.com>
2021-06-14 13:46:08 +0200

    Revert the previous commit of a label sorting scheme

--- middleware/cam/app/CamOpt.hs
+++ middleware/cam/app/CamOpt.hs
@@ -39,13 +39,11 @@ module CamOpt ( Exp (..)
 import Control.Monad (replicateM)
 import Data.Foldable (fold)
 import Data.Int (Int32)
-import Data.List (sortBy)
 import Data.Word (Word8)
 import Prelude hiding (lookup)
 import qualified Control.Monad.State.Strict as S
 import qualified Data.Set as Set
 
-import Debug.Trace
 -- XXX: When adding new primitives like RTS3 do not forget
 --      to ensure the rFreeSys function is extended as well
 
@@ -255,10 +253,7 @@ freshLabel = do
 
 
 interpret :: Exp -> CAM
-interpret e =  instrs
-           <+> Ins STOP
-           <+> fold (sortByLabel thunks_)
-           <+> labelGraveyard
+interpret e =  instrs <+> Ins STOP <+> fold thunks_ <+> labelGraveyard
   where
     (instrs, CodegenState {thunks = thunks_} ) =
       S.runState
@@ -613,10 +608,6 @@ zipWith3A ::   Applicative t
           -> t [d]
 zipWith3A f xs ys zs = sequenceA (zipWith3 f xs ys zs)
 
-sortByLabel :: [CAM] -> [CAM]
-sortByLabel =
-  sortBy (\(Lab l1 _) (Lab l2 _) -> compare l1 l2) . concat . map splitLabels
-
 ------- r-free and r-closed expressions--------
 
 type PSet = Set.Set -- synonym for powerset
@@ -730,57 +721,3 @@ The difference occurs in the type
 The `lookup` function has a special case for this
 pattern.
 -}
-
-
-
-
-
-
-
-
-
-
-
-
-data FlatCAM = Plain CAM
-             | Labeled Label CAM
-             deriving (Ord, Show, Eq)
-
-flattenCAM :: CAM -> [FlatCAM]
-flattenCAM (Ins i) = [Plain $ Ins i]
-flattenCAM (Seq c1 c2) = flattenCAM c1 ++ flattenCAM c2
-flattenCAM (Lab l c1)  =
-  case (head f) of
-    (Plain (Ins instr)) -> Labeled l (Ins instr) : tail f
-    _ -> error "Failure in flattening"
-  where
-    f = flattenCAM c1
-
-rebuildCAM :: [FlatCAM] -> CAM
-rebuildCAM [] = error "Failure when rebuilding CAM"
-rebuildCAM [(Plain c)] = c
-rebuildCAM [(Labeled l c)] = Lab l c
-rebuildCAM (x:xs) =
-  case x of
-    Plain c -> Seq c (rebuildCAM xs)
-    Labeled l c -> Lab l (Seq c (rebuildCAM xs))
-
--- The `show` is necessary because `rebuildCAM` simplifies the
--- structure of the CAM tree made of `Seq`. `rebuildCAM` essentially
--- makes CAM a fully right leaning linked list. The original
--- CAM data type might have had some more complex structure.
-verify :: CAM -> Bool
-verify c = (show . rebuildCAM . flattenCAM) c == (show c)
-
-
-splitLabels :: CAM -> [CAM]
-splitLabels cam = map rebuildCAM flatcam'
-  where
-    flatcam  = flattenCAM cam
-    flatcam' = splitLabels' [] [head flatcam] (tail flatcam)
-
-splitLabels' :: [[FlatCAM]] -> [FlatCAM] -> [FlatCAM] -> [[FlatCAM]]
-splitLabels' xs ys [] = xs ++ [ys]
-splitLabels' xs ys (Labeled l c:zs) =
-  splitLabels' (xs ++ [ys]) [(Labeled l c)] zs
-splitLabels' xs ys (z:zs) = splitLabels' xs (ys ++ [z]) zs
