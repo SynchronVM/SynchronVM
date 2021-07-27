8d42c56e43135e5858337656f63cd70866ab183d
Joel Svensson <svenssonjoel@yahoo.se>
2021-07-27 19:25:33 +0200

    set up some a chibios project to experiment with talking to the audio dac

---
+++ experiments/chibios/audio-dac/main.c
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+
+#include "ch.h"
+#include "hal.h"
+#include "usbcfg.h"
+#include "chprintf.h"
+
+BaseSequentialStream *chp = NULL;
+
+int main(void) {
+  halInit();
+  chSysInit();
+
+  sduObjectInit(&SDU1);
+  sduStart(&SDU1, &serusbcfg);
+
+  /*
+   * Activates the USB driver and then the USB bus pull-up on D+.
+   * Note, a delay is inserted in order to not have to disconnect the cable
+   * after a reset.
+   */
+  usbDisconnectBus(serusbcfg.usbp);
+  chThdSleepMilliseconds(1500);
+  usbStart(serusbcfg.usbp, &usbcfg);
+  usbConnectBus(serusbcfg.usbp);
+  chThdSleepMilliseconds(500);
+
+
+  chThdSleepMilliseconds(2000);
+  chp = (BaseSequentialStream*)&SDU1;
+
+  while(true) {
+    chprintf(chp,"Hello world\r\n");
+    chThdSleepMilliseconds(1000);
+  }
+
+  return 0; //unreachable
+}
