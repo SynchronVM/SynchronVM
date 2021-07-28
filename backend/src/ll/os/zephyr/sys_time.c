fae5ad5a5f988949e7a075cf18b92754d088843b
Joel Svensson <svenssonjoel@yahoo.se>
2021-07-28 22:37:51 +0200

    sketching out more of an implementation of low-level time

--- backend/src/ll/os/zephyr/sys_time.c
+++ backend/src/ll/os/zephyr/sys_time.c
@@ -23,30 +23,86 @@
 /**********************************************************************************/
 
 #include <zephyr.h>
+#include <zephyr/types.h>
+#include <drivers/counter.h>
 
 #include <ll/ll_sys_time.h>
 #include <hal/zephyr/svm_zephyr.h>
 
+#define COUNTER DT_LABEL(DT_ALIAS(svm_sys_timer))
+
+struct counter_alarm_cfg alarm_cfg;
+struct counter_top_cfg overflow_cfg; 
+
+const struct device *counter_dev = NULL;
+
 static zephyr_interop_t *zephyr_interop;
 
+static uint32_t      counter_high_word;
+static uint32_t      counter_freq;
+
+
+void alarm_callback(const struct device *dev, uint8_t chan, uint32_t ticks, void *user_data) {
+  /* send a message via the interop */
+}
+
+void overflow_callback(const struct device *dev, void* user_data) {
+  counter_high_word ++;
+}
+
+
+/**************************************/
+/* Interface functions implementation */
+
 bool ll_sys_time_init(void *os_interop) {
+
+  counter_high_word = 0;
+  
   zephyr_interop = (zephyr_interop_t*)os_interop;
-  return (bool)zephyr_interop; 
+  if (!zephyr_interop) return false;
+ 
+  counter_dev = device_get_binding(COUNTER);
+  if (!counter_dev) return false;
+
+  counter_freq = counter_get_frequency(counter_dev);
+
+  overflow_cfg.ticks = UINT_MAX;
+  overflow_cfg.callback = overflow_callback;
+  overflow_cfg.flags = 0;
+  overflow_cfg.user_data = NULL;
+  
+  if (!counter_set_top_value(counter_dev, &overflow_cfg)) return false;
+
+  /* just prepare the alarm config. Dont actually set this alarm. */
+  alarm_cfg.flags = COUNTER_ALARM_CFG_ABSOLUTE | COUNTER_ALARM_CFG_EXPIRE_WHEN_LATE;
+  alarm_cfg.ticks = 0;
+  alarm_cfg.callback = alarm_callback;
+  alarm_cfg.user_data = &alarm_cfg;
+
+  /* an alarm in the past should go off immediately */
+  if (!counter_set_guard_period(counter_dev, UINT_MAX/2, COUNTER_GUARD_PERIOD_LATE_TO_SET))
+    return false;
+
+  if (counter_start(counter_dev) != 0) return false;
+  
+  return true;
 }
 
 ll_sys_time_t ll_sys_time_get_current_ticks(void) {
 
   ll_sys_time_t time;
-  time.low_word = 0;
-  time.high_word = 0;
-
-  return time;
   
+  uint32_t key = irq_lock();
+  counter_get_value(counter_dev, &time.low_word);
+  time.high_word = counter_high_word;
+  irq_unlock(key);
+  
+  return time;
 }
 
 
 uint32_t ll_sys_time_get_clock_freq(void) {
-  return 0;
+  return counter_freq;
 }
 
 
