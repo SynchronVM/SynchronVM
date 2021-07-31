0a1861f9deb404b3effeae1644b6d7eaa374e75c
Joel Svensson <svenssonjoel@yahoo.se>
2021-07-31 18:07:51 +0200

    preparation for ll revamping

--- backend/src/ll/os/chibios/button.c
+++ backend/src/ll/os/chibios/button.c
@@ -23,6 +23,7 @@
 /**********************************************************************************/
 
 #include <button.h>
+#include <ll/ll_sys_time.h>
 
 #include <hal_pal.h>
 uint32_t button_num(void){
@@ -41,7 +42,8 @@ static void button_cb(void *arg) {
   
   ll_driver_msg_t msg; 
   msg.driver_id = button->id; 
-  msg.timestamp = 0; //ll_driver_timestamp();
+  msg.timestamp.high_word = 0; //ll_driver_timestamp();
+  msg.timestamp.low_word = 0;
   msg.data = state;  // 1 or 0
 
   if (button->interop->send_message(button->interop, msg) == -1) {
@@ -75,4 +77,58 @@ button_driver_t *button_init(uint32_t drv_id, void *backend_custom, uint32_t ide
   }
   return r;
 
+}
+
+#include <ll/ll_button.h>
+
+
+static uint32_t ll_button_control(struct ll_driver_s *this, uint8_t *data, uint32_t data_size) {
+  return 0;
+}
+
+static uint32_t ll_button_data_available(struct ll_driver_s *this) {
+  return 1;
+}
+
+static uint32_t ll_button_data_writeable(struct ll_driver_s *this) {
+  return 0;
+}
+
+static uint32_t ll_button_read(struct ll_driver_s *this, uint8_t *data, uint32_t data_size) {
+  button_driver_t *b = (button_driver_t*)this->driver_info;
+
+  uint32_t r = 0;
+  
+  if (data_size == 4) {
+    data[0] = b->state;
+    data[1] = b->state >> 8;
+    data[2] = b->state >> 16;
+    data[3] = b->state >> 24;
+    r = 4;
+  }
+  
+  return r;
+}
+
+static uint32_t ll_button_write(struct ll_driver_s *this, uint8_t *data, uint32_t data_size) {
+  return 0;
+}
+
+bool ll_button_init(ll_driver_t* lld, uint32_t drv_id, void* backend_custom,  uint32_t button_id) {
+
+  button_driver_t *button_driver = button_init(drv_id, backend_custom, button_id);
+
+  bool r = false;
+  
+  if (button_driver) {
+    r = true; 
+    lld->driver_info = (void*) button_driver;
+    lld->is_synchronous = false;
+    lld->ll_control_fun = ll_button_control;
+    lld->ll_read_fun = ll_button_read;
+    lld->ll_write_fun = ll_button_write;
+    lld->ll_data_readable_fun = ll_button_data_available;
+    lld->ll_data_writeable_fun = ll_button_data_writeable;
+  }
+  return r;
 }
--- backend/src/ll/os/chibios/led.c
+++ backend/src/ll/os/chibios/led.c
@@ -62,3 +62,61 @@ void led_set(led_driver_t *led, bool value) {
 bool led_state(led_driver_t *led) {
   return led->state;
 }
+
+/* LL interface implementation */
+
+#include <ll/ll_led.h>
+#include <string.h>
+
+static uint32_t ll_led_control(struct ll_driver_s *this, uint8_t *data, uint32_t data_size) {
+  return 0;
+}
+
+static uint32_t ll_led_data_available(struct ll_driver_s *this) {
+  return 1;
+}
+
+static uint32_t ll_led_data_writeable(struct ll_driver_s *this) {
+  return 1;
+}
+
+/* sets bit 0 in the first byte of data to the value of the led.
+    The rest of data is cleared. */ 
+static uint32_t ll_led_read(struct ll_driver_s *this, uint8_t *data, uint32_t data_size) {
+  led_driver_t *led_driver = (led_driver_t*)this->driver_info; 
+  bool state = led_state(led_driver);
+
+  if (data_size > 0) {
+    memset(data,0, data_size);
+    data[0] = state?1:0;
+  }
+  return data_size;
+}
+
+/* data[0] will be reinterpreted as a bool */
+static uint32_t ll_led_write(struct ll_driver_s *this, uint8_t *data, uint32_t data_size) {
+  led_driver_t *led_driver = (led_driver_t*)this->driver_info;
+  if (data_size > 0) {
+    led_set(led_driver, data[0]); 
+  }
+  return data_size; /* there are alternative interpretations one could make... */
+}
+
+bool ll_led_init(ll_driver_t* lld, uint32_t led_id, bool initial_state) {
+  led_driver_t *led_driver = led_init(led_id);
+  bool r = false;
+  
+  if (led_driver) {
+    r = true; 
+    led_set(led_driver, initial_state);
+    lld->driver_info = (void*) led_driver;
+    lld->is_synchronous = true;
+    lld->ll_control_fun = ll_led_control;
+    lld->ll_read_fun = ll_led_read;
+    lld->ll_write_fun = ll_led_write;
+    lld->ll_data_readable_fun = ll_led_data_available;
+    lld->ll_data_writeable_fun = ll_led_data_writeable;
+  }
+  return r;
+}
+
