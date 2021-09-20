1eb38ac885246e5d1516fbdad25c1774688a1ed9
Joel Svensson <svenssonjoel@yahoo.se>
2021-09-20 09:24:32 +0200

    moved more platform specific files to the platform subtree

---
+++ backend/platform/linux-x86/src/sys_debug_uart.c
@@ -0,0 +1,37 @@
+/**********************************************************************************/
+/* MIT License									  */
+/* 										  */
+/* Copyright (c) 2021 Joel Svensson, Abhiroop Sarkar 				  */
+/* 										  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy	  */
+/* of this software and associated documentation files (the "Software"), to deal  */
+/* in the Software without restriction, including without limitation the rights	  */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell	  */
+/* copies of the Software, and to permit persons to whom the Software is	  */
+/* furnished to do so, subject to the following conditions:			  */
+/* 										  */
+/* The above copyright notice and this permission notice shall be included in all */
+/* copies or substantial portions of the Software.				  */
+/* 										  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR	  */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,	  */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE	  */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER	  */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  */
+/* SOFTWARE.									  */
+/**********************************************************************************/
+
+#include <sys/sys_debug_uart.h>
+#include <stdio.h>
+
+void debug_uart_init(uint32_t buffer_size) {
+  return;
+}
+
+int debug_printf(const char *format, ...) {
+   va_list vargs;
+   va_start(args, format);
+   vprintf(fmt, vargs);
+   va_end(vargs);
+}
---
+++ backend/platform/linux-x86/src/sys_time.c
@@ -0,0 +1,62 @@
+/**********************************************************************************/
+/* MIT License									  */
+/* 										  */
+/* Copyright (c) 2021 Joel Svensson, Abhiroop Sarkar 				  */
+/* 										  */
+/* Permission is hereby granted, free of charge, to any person obtaining a copy	  */
+/* of this software and associated documentation files (the "Software"), to deal  */
+/* in the Software without restriction, including without limitation the rights	  */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell	  */
+/* copies of the Software, and to permit persons to whom the Software is	  */
+/* furnished to do so, subject to the following conditions:			  */
+/* 										  */
+/* The above copyright notice and this permission notice shall be included in all */
+/* copies or substantial portions of the Software.				  */
+/* 										  */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR	  */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,	  */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE	  */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER	  */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  */
+/* SOFTWARE.									  */
+/**********************************************************************************/
+
+#include <sys/sys_time.h>
+
+bool sys_time_init(void *os_interop){
+  // TODO: Implement this;
+  (void)os_interop;
+  return true;
+}
+
+Time sys_time_get_current_ticks(void){
+  // TODO: Implement this;
+  return 1;
+}
+
+uint32_t sys_time_get_clock_freq(void){
+  // TODO: Implement this;
+  return 1;
+}
+
+
+bool sys_time_set_wake_up(Time absolute){
+  // TODO: Implement this;
+  (void)absolute;
+  return true;
+}
+
+Time sys_get_wake_up_time(void){
+  // TODO: Implement this;
+  return 1;
+}
+
+bool sys_is_alarm_set(void){
+  // TODO: Implement this;
+  return false;
+}
+
+
+
+void sys_sleep_ms(uint32_t ms){ (void)ms; }
