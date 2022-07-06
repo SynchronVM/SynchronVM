da2d5d322e166d057f951117f6a23eb583af9a64
Abhiroop <asiamgenius@gmail.com>
2022-07-06 16:42:12 +0200

    Add first working example of FFI with all the infrastructure

--- backend/tests/rts/FOREIGN/foreign.c
+++ backend/tests/rts/FOREIGN/foreign.c
@@ -1,20 +1,10 @@
 #include<foreign.h>
 #include<typedefs.h>
 cam_value_t foreign_add(cam_value_t a, cam_value_t b){
-  (void)a;
-  (void)b;
   cam_value_t v;
   v.flags = 0;
-  v.value = 0;
+  v.value = a.value + b.value;
   return v;
 }
 
-cam_value_t foreign_sub(cam_value_t a, cam_value_t b){
-  (void)a;
-  (void)b;
-  cam_value_t v;
-  v.flags = 0;
-  v.value = 0;
-  return v;
-}
 
--- backend/tests/rts/FOREIGN/foreign.h
+++ backend/tests/rts/FOREIGN/foreign.h
@@ -4,6 +4,5 @@
 #include<typedefs.h>
 
 cam_value_t foreign_add(cam_value_t a, cam_value_t b);
-cam_value_t foreign_sub(cam_value_t a, cam_value_t b);
 
 #endif
